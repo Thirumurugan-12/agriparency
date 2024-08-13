c2c0a13e1c294d709a3e674022e9d25108b370eb
Thirumurugan-12 <sec22cs123@sairamtap.edu.in>
2024-08-13 22:27:52 +0530

    added api call

--- lib/screens/buystock.dart
+++ lib/screens/buystock.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 
 class BuyStockPage extends StatefulWidget {
@@ -9,12 +11,58 @@ class BuyStockPage extends StatefulWidget {
 
 class _BuyStockPageState extends State<BuyStockPage> {
   final TextEditingController batchIdController = TextEditingController();
-  final TextEditingController producerAddressController = TextEditingController();
+  final TextEditingController producerAddressController =
+      TextEditingController();
   final TextEditingController statusController = TextEditingController();
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  Future<void> createBatch() async {
+    final response = await http.post(
+      Uri.parse('https://b990-117-254-38-161.ngrok-free.app/create_batch'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, String>{
+        'batch_id': batchIdController.text,
+        'producer': producerAddressController.text,
+        'status': statusController.text,
+      }),
+    );
+
+    if (response.statusCode == 200) {
+      // If the server returns a 200 OK response, parse the JSON.
+      Map<String, dynamic> result = jsonDecode(response.body);
+      print('Transaction hash: ${result['transaction_hash']}');
+      print('Receipt to: ${result['receipt_to']}');
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Transaction Successful'),
+            content: Text('Transaction is successful and pushed in market',
+                style: TextStyle(color: Colors.black)),
+            actions: <Widget>[
+              ElevatedButton(
+                child: Text('OK'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          );
+        },
+      );
+    } else {
+      // If the server did not return a 200 OK response, throw an exception.
+      print(response.body);
+      throw Exception('Failed to create batch.');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
       appBar: AppBar(
         title: Text('Buy Stock'),
       ),
@@ -44,8 +92,7 @@ class _BuyStockPageState extends State<BuyStockPage> {
             SizedBox(height: 20),
             ElevatedButton(
               onPressed: () {
-                // Implement your functionality for buying a stock here
-                // You can access the text of the inputs with batchIdController.text, producerAddressController.text, and statusController.text
+                createBatch();
               },
               child: Text('Confirm Purchase'),
             ),
